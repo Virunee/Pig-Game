c5f543022cc6cfd8a415c748763db295ffb677f6
Virunee <viravongv@gmail.com>
2017-10-04 10:49:08 +0100

    Added init function
    Added init() function and added functionality to the reset button.

--- app.js
+++ app.js
@@ -8,20 +8,35 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
+function init() {
+  scores = [0,0];
+  roundScore = 0;
+  activePlayer = 0;
 
-document.querySelector('.dice').style.display = 'none';
+  document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.getElementById('name-0').textContent = "Player 1";
+  document.getElementById('name-1').textContent = "Player 2";
+
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+
+  document.querySelector('.player-0-panel').classList.add('active');
+}
+
 
 // When someone clicks the 'roll' button
 document.querySelector('.btn-roll').addEventListener('click', function() {
@@ -52,7 +67,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
     // Check if player has won the game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       document.querySelector('#name-' + activePlayer).textContent = "Winner!";
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -62,6 +77,8 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     }
 
   });
+
+  document.querySelector('.btn-new').addEventListener('click', init);
 
 function nextPlayer() {
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
